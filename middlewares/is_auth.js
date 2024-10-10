e939a49de44ced87e7f7267d271d5c9efc35ef93
RamadanCRaji <armadanraji@gmail.com>
2024-10-10 11:25:35 -0500

    debug:updated middleware structure for auth

--- middlewares/is_auth.js
+++ middlewares/is_auth.js
@@ -4,23 +4,28 @@ dotenv.config({ path: "./config/config.env" });
 
 module.exports = {
    authorizer: function (req, res, next) {
-      console.log("i made it here");
       const authHeader = req.header("Authorization");
       if (!authHeader) {
-         console.log("something went wrong in auth path");
+         console.log("Authorization Header is missing");
          return res.status(401).json({ error: "Unauthorized, Access Denied" });
       }
 
-      const access_token = authHeader.split(" ")[1];
+      const [scheme, access_token] = authHeader.split(" ");
+      if (scheme !== "Bearer" || !access_token) {
+         console.log("Invalid authorization format");
+         return res.status(401).json({ error: "Invalid authorization format" });
+      }
 
       try {
          // Verify the token and save it in the variable
          const secret = process.env.SECRET;
+         console.log("Secret Key:", secret);
+
          const decodedToken = jwt.verify(access_token, secret);
 
          // Attach the decoded token to the request object for further use
          req.user = decodedToken;
-         console.log({ decodedToken });
+         console.log("Decoded Token:", decodedToken);
 
          next();
       } catch (err) {
