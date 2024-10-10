c6f8c23743633db6087ad551419089811218be31
RamadanCRaji <armadanraji@gmail.com>
2024-10-10 10:36:37 -0500

    debug: added error.message to auth path

--- middlewares/is_auth.js
+++ middlewares/is_auth.js
@@ -6,8 +6,8 @@ module.exports = {
    authorizer: function (req, res, next) {
       console.log("i made it here");
       const authHeader = req.header("Authorization");
-      console.log({ authHeader });
       if (!authHeader) {
+         console.log("something went wrong in auth path");
          return res.status(401).json({ error: "Unauthorized, Access Denied" });
       }
 
